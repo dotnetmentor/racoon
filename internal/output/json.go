f13d20b7c8cbc9539f49a1e095408ed4b987e2c4
Kristoffer Ahl <mail@77dynamite.com>
2022-02-08 22:52:15 +0100

    feat: select secrets for export using flags for export command

--- internal/output/json.go
+++ internal/output/json.go
@@ -4,21 +4,19 @@ import (
 	"encoding/json"
 	"io"
 	"strings"
-
-	"github.com/dotnetmentor/racoon/internal/config"
 )
 
-func Json(w io.Writer, m config.Manifest, keys map[string]string, values map[string]string) {
+func Json(w io.Writer, secrets []string, remap map[string]string, values map[string]string) {
 	jo := map[string]string{}
-	for _, s := range m.Secrets {
+	for _, s := range secrets {
 		var key string
-		if remapped, ok := keys[s.Name]; ok && remapped != "" {
+		if remapped, ok := remap[s]; ok && remapped != "" {
 			key = remapped
 		} else {
-			key = s.Name
+			key = s
 		}
 
-		value := strings.TrimSuffix(values[s.Name], "\n")
+		value := strings.TrimSuffix(values[s], "\n")
 		jo[key] = value
 	}
 	json.NewEncoder(w).Encode(jo)
